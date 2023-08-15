33d14cf50693cee230309a64eebae9aa41e83502
skabeo <spencerokyere95@gmail.com>
2023-08-15 01:07:58 +0000

    Create react index entry path

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,18 @@
-// Entry point for the build script in your package.json
-import "@hotwired/turbo-rails"
-import "./controllers"
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/App';
+import { Provider } from 'react-redux';
+import store from './redux/store';
+
+function Index() {
+  return (
+    <Provider store={store}>
+      <App />
+    </Provider>
+  );
+}
+
+ReactDOM.render(
+  <Index/>,
+  document.getElementById('root'),
+);
